21a01584fc84a256235033e27bb756d0018727fd
Alejo Martina <alejomartina477@gmail.com>
2024-06-19 15:30:51 -0300

    update index.js

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,13 +1,14 @@
 import express from "express";
 import usersRouter from "./routes/users.routes.js";
 import ejerciciosRouter from "./routes/ejercicios.routes.js";
-
+import membresiasRouter from "./routes/membresias.routes.js";
 
 const app = express();
 const PORT = 3000;
 
 app.use(express.json()); //para usar json en el body
 app.use(usersRouter);
+app.use(membresiasRouter);
 app.use(ejerciciosRouter);
 
 app.listen(PORT, () => {
