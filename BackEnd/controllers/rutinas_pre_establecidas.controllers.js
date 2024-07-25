410330ef0d2a7e4f018afc432ca30cd7130f6e83
Alejo Martina <alejomartina477@gmail.com>
2024-07-25 19:08:46 -0300

    update rutinas pre-establecidas

--- BackEnd/controllers/rutinas_pre_establecidas.controllers.js
+++ BackEnd/controllers/rutinas_pre_establecidas.controllers.js
@@ -1,7 +1,7 @@
 import {pool} from "../bd.js";
 
 export const getRutinas_pre_establecidasBySexoNroDias = async (req, res) => {
-  
+
   try{
     const { sexo, nrodias } = req.params;
     const [result] = await pool.query("SELECT lrp.* , ej.nombre FROM rutinas_pre_establecidas rp  inner join lineas_rutina_pre_establecida lrp on rp.sexo = lrp.sexo and rp.nroDias = lrp.nroDias inner join ejercicios ej on lrp.codEjercicio = ej.codEjercicio WHERE rp.sexo = ? and rp.nroDias = ? order by orden", [sexo, nrodias]);
@@ -12,5 +12,16 @@ export const getRutinas_pre_establecidasBySexoNroDias = async (req, res) => {
     }
   } catch (error){
     console.log(error);
+  }
+};
+
+export const updateRutinas_pre_establecidas = async (req, res) => {
+  try {
+    const [result] = await pool.query("INSERT INTO lineas_rutina_pre_establecida (sexo, nroDias, orden, codEjercicio, series, repeticiones) VALUES (?, ?, ?, ?, ?, ?)", [req.body.sexo, req.body.nroDias, req.body.orden, req.body.codEjercicio, req.body.series, req.body.repeticiones]);
+    if (result.affectedRows === 0) {
+      return res.status(404).json({message:"no se encuentra la linea de rutina pre establecida."});
+    } res.json({message:"linea de rutina pre establecida actualizada correctamente."});
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
--- BackEnd/routes/rutinas_pre_establecidas.routes.js
+++ BackEnd/routes/rutinas_pre_establecidas.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import {
   getRutinas_pre_establecidasBySexoNroDias, //no estoy seguro si se filtan por id
+  updateRutinas_pre_establecidas,
 } from "../controllers/rutinas_pre_establecidas.controllers.js";
 
 
 const router = Router();
 
 router.get("/api/rutinas_pre_establecidas/:sexo/:nrodias", getRutinas_pre_establecidasBySexoNroDias);
+
+router.put("/api/rutinas_pre_establecidas", updateRutinas_pre_establecidas);
 
 export default router;
