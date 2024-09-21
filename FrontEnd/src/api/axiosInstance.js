f9fa16e6fab71cd1efd64f67d446a4cf850e79ae
Matias Luhmann <139171817+Lumansito@users.noreply.github.com>
2024-09-21 17:12:26 -0300

    .

--- FrontEnd/src/api/axiosInstance.js
+++ FrontEnd/src/api/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.100.8:3000/api', 
+  baseURL: 'http://localhost:3000/api', //coloar la ip de la maquina donde se esta ejecutando el backend
 });
 
 
