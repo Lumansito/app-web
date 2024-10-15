d48234c59264fb222bc0f8465b4ddd53012e3c35
Matias Luhmann <139171817+Lumansito@users.noreply.github.com>
2024-10-15 17:16:11 -0300

    Modificaciones en el home(estetica)

--- FrontEnd/src/pages/Home.jsx
+++ FrontEnd/src/pages/Home.jsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
-import { LogIn } from "./LogIn"
-import { useUsuario } from "../context/Usuario/UsuarioProvider"
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { LogIn } from "./LogIn";
+import { useUsuario } from "../context/Usuario/UsuarioProvider";
 
-export  function Home() {
-  const { rol, comprobarToken, dni, logout } = useUsuario()
-  const navigate = useNavigate()
-  const [loading, setLoading] = useState(false)
+export function Home() {
+  const { rol, comprobarToken, dni, logout } = useUsuario();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    comprobarToken()
-  }, [])
+    comprobarToken();
+  }, []);
 
   const handleLogout = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
-      await logout()
-      alert("Has cerrado sesión exitosamente.")
-      navigate("/")
+      await logout();
+      alert("Has cerrado sesión exitosamente.");
+      navigate("/");
     } catch (error) {
-      alert("No se pudo cerrar la sesión. Intenta de nuevo.")
+      alert("No se pudo cerrar la sesión. Intenta de nuevo.");
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   const handleNavigation = (path) => {
-    navigate(path)
-  }
+    navigate(path);
+  };
 
   if (rol.length === 0) {
-    return <LogIn />
+    return <LogIn />;
   }
 
   return (
@@ -38,48 +38,78 @@ export  function Home() {
       <h1 className="text-2xl font-bold text-center mb-6">Gym</h1>
       <div className="max-w-sm mx-auto bg-gray-100 rounded-lg shadow-md overflow-hidden">
         <div className="p-4 border-b border-gray-200">
-          <h2 className="text-lg font-semibold">Bienvenido, usuario con DNI: {dni}</h2>
+          <h2 className="text-lg font-semibold">
+            Bienvenido, usuario con DNI: {dni}
+          </h2>
         </div>
         <div className="p-4 space-y-4">
           <p className="text-sm font-medium">Tus roles:</p>
           <div className="space-y-2">
             {rol.includes(1) && (
               <div className="space-y-2">
-                <span className="block text-sm bg-gray-200 p-2 rounded">Cliente</span>
-                <button
-                  className="w-full py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors"
-                  onClick={() => handleNavigation("/clases")}
-                >
-                  Realizar Reserva
-                </button>
-                <button
-                  className="w-full py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors"
-                  onClick={() => handleNavigation("/rutinas")}
-                >
-                  Mis Rutinas
-                </button>
+                <span className="block text-sm bg-gray-200 p-2 rounded">
+                  Cliente
+                </span>
+                <div className="flex space-x-2">
+                  <button
+                    className="flex-1 py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors max-h-40"
+                    onClick={() => handleNavigation("/clases")}
+                  >
+                    <img
+                      src="src/img/calendar.png"
+                      alt="Calendario"
+                      className="mx-auto w-16 h-16"
+                    />
+                    <h1>Reservas</h1>
+                  </button>
+                  <button
+                    className="flex-1 py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors"
+                    onClick={() => handleNavigation("/rutinas")}
+                  >
+                    <img
+                      src="src/img/rutina.png"
+                      alt="Rutina"
+                      className="mx-auto w-14 "
+                    />
+                    <h1>Mis Rutinas</h1>
+                  </button>
+                </div>
               </div>
             )}
             {rol.includes(2) && (
               <div className="space-y-2">
-                <span className="block text-sm bg-gray-200 p-2 rounded">Profesional</span>
-                <button
-                  className="w-full py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors"
-                  onClick={() => handleNavigation("/rutinas/solicitudes")}
-                >
-                  Solicitudes de Rutina
-                </button>
-                <button
-                  className="w-full py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors"
-                  onClick={() => handleNavigation("/seguimiento/lista")}
-                >
-                  Marcas Clientes
-                </button>
+                <span className="block text-sm bg-gray-200 p-2 rounded">
+                  Profesional
+                </span>
+                <div className="flex space-x-2">
+                  {" "}
+                  {/* Cambiado a flexbox */}
+                  <button
+                    className="flex-1 py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors"
+                    onClick={() => handleNavigation("/rutinas/solicitudes")}
+                  >
+                    <img
+                      src="src/img/solicitudes.png"
+                      alt="Solicitudes"
+                      className="mx-auto w-14 "
+                    />
+                    <h1>Solicitudes de Rutina</h1>
+                  </button>
+                  <button
+                    className="flex-1 py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors"
+                    onClick={() => handleNavigation("/seguimiento/lista")}
+                  >
+                    Marcas Clientes
+                  </button>
+                </div>
               </div>
             )}
+
             {rol.includes(3) && (
               <div className="space-y-2">
-                <span className="block text-sm bg-gray-200 p-2 rounded">Admin</span>
+                <span className="block text-sm bg-gray-200 p-2 rounded">
+                  Admin
+                </span>
                 <button
                   className="w-full py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors"
                   onClick={() => handleNavigation("/cupos/lista")}
@@ -96,7 +126,7 @@ export  function Home() {
                   className="w-full py-2 px-4 bg-gray-200 text-black rounded hover:bg-gray-300 transition-colors"
                   onClick={() => handleNavigation("/ejercicios")}
                 >
-                  Ejercicios 
+                  Ejercicios
                 </button>
               </div>
             )}
@@ -120,5 +150,5 @@ export  function Home() {
         </div>
       </div>
     </div>
-  )
+  );
 }
